6a4b752ccd3eb00a4f9c871af595cadda0011b4e
Olle-Nilsson <Olle.Nilsson.2005@gmail.com>
2024-10-10 17:28:39 +0200

    Changed information managing

--- Person.cs
+++ Person.cs
@@ -11,6 +11,49 @@ public class Person
     public string SocialSecurityNumber { get; set; } = "";
     public string ZipCode { get; set; } = "";
 
+
+    public void GetInfo(int personId)
+    {
+        switch (personId)
+        {
+            case 1:
+                Address = "DummyVägen 2";
+                City = "Mamlö";
+                FirstName = "Bengt";
+                LastName = "Nilsson";
+                PhoneNumber = "075-9432744";
+                SocialSecurityNumber = "19830524-6786";
+                ZipCode = "250-00";
+                break;
+            case 2:
+                Address = "DummyGatan 7";
+                City = "Mamlö";
+                FirstName = "Greta";
+                LastName = "Olsson";
+                PhoneNumber = "077-7349656";
+                SocialSecurityNumber = "19531002-45";
+                ZipCode = "250-00";
+                break;
+            case 3:
+                Address = "DummyVägen 5";
+                City = "Mamlö";
+                FirstName = "Olof";
+                LastName = "Persson";
+                PhoneNumber = "071-4849784";
+                SocialSecurityNumber = "20020625-6374";
+                ZipCode = "250-00";
+                break;
+            case 4:
+                Address = "DummyGatan 5";
+                City = "Mamlö";
+                FirstName = "Nils";
+                LastName = "Nilssson";
+                PhoneNumber = "072-9839286";
+                SocialSecurityNumber = "20051201-3840";
+                ZipCode = "250-01";
+                break;
+        }
+    }
     public override string ToString()
     {
         return $"Name: {FirstName} {LastName} PhoneNumber: {PhoneNumber} SocialSecurityNumber: {SocialSecurityNumber} Address: {Address} {City} {ZipCode}";
--- Program.cs
+++ Program.cs
@@ -4,46 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        Student student = new Student();
+        student.GetInfo(1);
+
         Teacher teacher = new Teacher();
-        teacher.Address = "DummyVägen 2";
-        teacher.City = "Mamlö";
-        teacher.FirstName = "Bengt";
-        teacher.LastName = "Nilsson";
-        teacher.PhoneNumber = "075-9432744";
-        teacher.SocialSecurityNumber = "19830524-6786";
-        teacher.ZipCode = "250-00";
+        teacher.GetInfo(2);
         teacher.FiledOfExpertise = "Programmering";
 
         CourseLeader courseLeader = new CourseLeader();
-        courseLeader.Address = "DummyGatan 7";
-        courseLeader.City = "Mamlö";
-        courseLeader.FirstName = "Greta";
-        courseLeader.LastName = "Olsson";
-        courseLeader.PhoneNumber = "077-7349656";
-        courseLeader.SocialSecurityNumber = "19531002-4584";
-        courseLeader.ZipCode = "250-00";
+        courseLeader.GetInfo(3);
         courseLeader.FiledOfExpertise = "Programmering";
         courseLeader.DateOfEmployment = DateTime.Now;
 
         Admin admin = new Admin();
-        admin.Address = "DummyVägen 5";
-        admin.City = "Mamlö";
-        admin.FirstName = "Olof";
-        admin.LastName = "Persson";
-        admin.PhoneNumber = "071-4849784";
-        admin.SocialSecurityNumber = "20020625-6374";
-        admin.ZipCode = "250-00";
+        admin.GetInfo(4);
         admin.FiledOfExpertise = "Programmering";
         admin.DateOfEmployment = DateTime.Now;
 
-        Student student = new Student();
-        student.Address = "DummyGatan 5";
-        student.City = "Mamlö";
-        student.FirstName = "Nils";
-        student.LastName = "Nilssson";
-        student.PhoneNumber = "072-9839286";
-        student.SocialSecurityNumber = "20051201-3840";
-        student.ZipCode = "250-01";
 
         Course course = new Course(teacher: teacher, courseLeader: courseLeader, admin: admin, numberOfWeeks: 16, startDate: DateTime.Now);
         course.CourseName = "Programmering i C#";
@@ -52,26 +29,18 @@ class Program
 
         Console.WriteLine(course);
 
+
+
         var path = string.Concat(Environment.CurrentDirectory, "/data/courses.json");
-        JsonManager.WriteJson(path, [course]);
 
 
-        var course1 = JsonManager.ReadJson(path);
-        foreach (var c in course1)
+        // JsonManager.WriteJson(path, [course]);
+
+        var courses = JsonManager.ReadJson(path);
+
+        foreach (var c in courses)
         {
             Console.WriteLine(c);
         }
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
